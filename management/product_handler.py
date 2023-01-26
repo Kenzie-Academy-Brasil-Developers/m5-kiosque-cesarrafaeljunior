af14875920a4c79591524dd84e26c4e709decbe2
César Rafael <cesarrdev@gmail.com>
2023-01-26 14:36:25 -0300

    feat: refactoring function get_product_by_id

--- management/product_handler.py
+++ management/product_handler.py
@@ -2,17 +2,18 @@ from menu import products
 
 
 def get_product_by_id(id):
+    if type(id) != int:
+        raise TypeError("product id must be an int")
     for product in products:
         if product["_id"] == id:
             return product
 
 
-def get_products_by_type(type):
-    array = []
-    for product in products:
-        if product["type"] == type:
-            array.append(product)
-    return array
+def get_products_by_type(type_product):
+    if type(type_product) != str:
+        raise TypeError("product type must be a str")
+    products_filter = [product for product in products if product["type"] == type_product]
+    return products_filter
 
 
 def add_product(menu, *args):
@@ -23,3 +24,27 @@ def add_product(menu, *args):
         new_id += 1
         menu.append(product)
     return product
+
+
+def menu_report():
+    products_count = len(products)
+    price = sum([product["price"] for product in products])
+    average_price = round(price / products_count, 2)
+    types_counts = {}
+
+    for product in products:
+        type_product = product["type"]
+        if type_product in types_counts:
+            types_counts[type_product] += 1
+        else:
+            types_counts[type_product] = 1
+
+    common_type = max(types_counts, key=types_counts.get)
+
+    report = {
+        "Products Count": products_count,
+        "Average Price": average_price,
+        "Common Type": common_type
+    }
+
+    return report
--- management/tab_handler.py
+++ management/tab_handler.py
@@ -7,11 +7,7 @@ def calculate_tab(account):
         product_consumed = get_product_by_id(product["_id"])
         product_consumed["amount"] = account[i]["amount"]
         products_consumed.append(product_consumed)
-    prices = 0
-    for product in products_consumed:
-        price = product["price"]
-        amount = product["amount"]
-        prices += price * amount
+    prices = sum([product["price"] * product["amount"] for product in products_consumed])
     subtotal = {
 
         "subtotal": f"${round(prices, 2)}"
